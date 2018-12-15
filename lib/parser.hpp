c2acdbaa9f0f23e55782b6b80976881cba4a5a7b
Wes Toleman <wesley.toleman@hotmail.com>
2018-12-15 12:54:42 +0800

    Add a parser for new line separated values

--- lib/parser.hpp
+++ lib/parser.hpp
@@ -26,4 +26,28 @@ namespace aoc
 			input.ignore(std::numeric_limits<std::streamsize>::max(), ',');
 		}
 	}
+
+	/**
+	 * @brief      Parses new line separated values
+	 *
+	 * @param      output          The an OutputIterator for a container to hold
+	 *                             the output output
+	 * @param      input           An input stream containing the new line
+	 *                             separated values
+	 *
+	 * @tparam     OutputIterator  An type that satisfies std `OutputIterator`
+	 */
+	template<class OutputIterator>
+	void parse_nlv(OutputIterator &output, std::istream &input)
+	{
+		using value_type = typename OutputIterator::container_type::value_type;
+
+		value_type tmp;
+
+		while (input >> tmp)
+		{
+			*output++ = tmp;
+			input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	}
 }
--- test/aoc_lib_tests.cpp
+++ test/aoc_lib_tests.cpp
@@ -50,3 +50,50 @@ TEST_CASE("CSV Input parser parses comma delimited `int`s")
 		REQUIRE(parse_values(ss) == expected);
 	}
 }
+
+TEST_CASE("New line separated input parser parses new line separated `int`s")
+{
+	const auto fill_stream = [](std::iostream& stream, const std::vector<int>& values)
+	{
+		for (const auto& val : values)
+		{
+			stream << val << '\n';
+		}
+	};
+
+	const auto parse_values = [](std::iostream& stream)
+	{
+		std::vector<int> ret;
+		auto inserter = std::back_inserter(ret);
+
+		aoc::parse_nlv(inserter, stream);
+
+		return ret;
+	};
+
+	std::stringstream ss;
+
+	SECTION("Parser handles positive numbers")
+	{
+		const std::vector<int> expected = { 0, 1, 2, 3, 4, 5 };
+		fill_stream(ss, expected);
+
+		REQUIRE(parse_values(ss) == expected);
+	}
+
+	SECTION("Parser handles negative numbers")
+	{
+		const std::vector<int> expected = { -1, -2, -3, -4, -5 };
+		fill_stream(ss, expected);
+
+		REQUIRE(parse_values(ss) == expected);
+	}
+
+	SECTION("Parser handles mixed positive and negative numbers")
+	{
+		const std::vector<int> expected = { 0, -1, 2, -3, -4, 5 };
+		fill_stream(ss, expected);
+
+		REQUIRE(parse_values(ss) == expected);
+	}
+}
